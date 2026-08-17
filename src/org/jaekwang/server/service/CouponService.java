df673611ce205e62487a3bc6710d0a6e55f9f9bd
jkpark <pjk163344@gmail.com>
2026-08-17 19:18:33 +0900

    feat: 전용 락 객체와 synchronized 블록을 활용한 동기화 제어 (v5.1)

--- src/org/jaekwang/server/service/CouponService.java
+++ src/org/jaekwang/server/service/CouponService.java
@@ -4,18 +4,25 @@ public class CouponService {
     public static int remainingCoupons = 100;
     public static int issuedCoupons = 0;
 
+    public static final Object lock = new  Object();
+
     public static String issue() {
-        int currentRemaining = remainingCoupons;
-        if (currentRemaining > 0) {
-            try {
-                Thread.sleep(8);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        synchronized (lock) {
+
+            int currentRemaining = remainingCoupons;
+            if (currentRemaining > 0) {
+                try {
+                    Thread.sleep(8);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                remainingCoupons = currentRemaining - 1;
+                issuedCoupons++;
+                return "Success";
             }
-            remainingCoupons = currentRemaining - 1;
-            issuedCoupons++;
-            return "Success";
+            return "Sold Out";
+
         }
-        return "Sold Out";
+
     }
 }
